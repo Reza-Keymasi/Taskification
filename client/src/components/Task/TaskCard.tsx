5a478a6884ba7269c9ef80a2c7e700b172307350
Reza-Keymasi <rezakeymasi1996@gmail.com>
2025-06-27 12:01:03 +0330

    create and design task card component to show taks details

---
+++ client/src/components/Task/TaskCard.tsx
@@ -0,0 +1,26 @@
+import Tag from "../UI/Tag";
+
+export default function Task() {
+  return (
+    <article className="flex flex-col p-3 gap-3 border border-sky-500 rounded-lg">
+      <div className="flex justify-between">
+        <p className="font-semibold text-xl">Title</p>
+        <div className="flex gap-4">
+          <Tag variant="outline" label="Due Date" />
+          <Tag variant="red" label="high" />
+        </div>
+      </div>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem cum ut
+        ducimus obcaecati in similique. Harum nulla impedit est, esse, ratione
+        doloribus, repellat molestiae culpa ut dicta ullam? Ipsam, ut!
+      </p>
+
+      <div className="flex justify-between">
+        <p>Progress Switch</p>
+        <p>Task Status</p>
+      </div>
+    </article>
+  );
+}
