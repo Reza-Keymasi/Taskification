08991eff0fd4decf6b655f7a322af686f5ba0314
Reza-Keymasi <rezakeymasi1996@gmail.com>
2025-07-01 15:40:17 +0330

    add month and year steps of date into drop down + add changing month and year functionality

--- client/src/components/UI/DatePicker.tsx
+++ client/src/components/UI/DatePicker.tsx
@@ -19,6 +19,8 @@ const months = [
 
 const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+type DateSteps = "days" | "months" | "years";
+
 type DatePickerProps = {
   containerClassName?: string;
   name: string;
@@ -35,10 +37,12 @@ export default function DatePicker({
   const [currentDate, setCurrentDate] = useState(new Date());
   const [monthStore, setMonthStore] = useState(getMonth);
   const [yearStore, setYearStore] = useState(getYear);
+  const [dateStep, setDateStep] = useState<DateSteps>("days");
 
   function handleToggleDropDown(e: MouseEvent<HTMLDivElement>) {
     e.preventDefault();
     setIsDropDownOpen((prev) => !prev);
+    setDateStep("days");
   }
 
   function calendarDays() {
@@ -82,6 +86,20 @@ export default function DatePicker({
 
   const calendarAllDays = calendarDays();
 
+  function toggleDateStep(step: DateSteps) {
+    setDateStep(step);
+  }
+
+  const yearOptions = Array.from({ length: 12 }, (_, i) => {
+    const year = new Date().getFullYear() - 4 + i;
+    return year;
+  });
+
+  function monthView() {
+    const date = new Date(yearStore, monthStore);
+    return date.toLocaleDateString("default", { month: "long" });
+  }
+
   return (
     <div className="relative w-[300px]">
       <Input
@@ -99,7 +117,7 @@ export default function DatePicker({
         //   }
         //   inputClassName={`${inputClassName} cursor-pointer`}
         //   isDisabled={isDisabled}
-        //   label={label}
+        // label={label}
         //   labelClassName={labelClassName}
         //   leftIconClassName={leftIconClassName}
         name={name}
@@ -118,7 +136,7 @@ export default function DatePicker({
 
       {isDropDownOpen && (
         <div
-          className="absolute w-full border border-gray-600 rounded-lg top-13"
+          className="absolute w-full min-h-[280px] border border-gray-600 rounded-lg top-13"
           onMouseDown={(e) => e.preventDefault()}
         >
           <div className="flex flex-col gap-5 py-4 px-4 w-full">
@@ -129,8 +147,18 @@ export default function DatePicker({
               </div>
 
               <div className="flex gap-1 font-medium justify-center">
-                <p>June,</p>
-                <p>2025</p>
+                <p
+                  className="cursor-pointer"
+                  onClick={() => toggleDateStep("months")}
+                >
+                  {monthView()},
+                </p>
+                <p
+                  className="cursor-pointer"
+                  onClick={() => toggleDateStep("years")}
+                >
+                  {yearStore}
+                </p>
               </div>
               <div className="flex items-center justify-center hover:bg-gray-400 w-7 h-7 rounded-full cursor-pointer">
                 <ChevronRight size={18} onClick={handleNextMonth} />
@@ -138,26 +166,69 @@ export default function DatePicker({
             </div>
 
             {/* This is days */}
-            <div className="grid grid-cols-7 gap-1  border-b border-gray-500">
-              {weekdays.map((weekday, index) => (
-                <div key={index} className="py-2 text-center text-sm">
-                  {weekday}
+            {dateStep === "days" && (
+              <div>
+                <div className="grid grid-cols-7 gap-1 text-[13px] border-b border-gray-500">
+                  {weekdays.map((weekday, index) => (
+                    <div key={index} className="py-1 text-center">
+                      {weekday}
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-            <div className="grid grid-cols-7 gap-1 text-center">
-              {calendarAllDays.map((day, index) => {
-                if (!day) return;
-                return (
-                  <div
-                    key={index}
-                    className="content-center w-8 h-8 rounded-full"
-                  >
-                    {day.getDate()}
-                  </div>
-                );
-              })}
-            </div>
+
+                <div className="grid grid-cols-7 gap-1 text-center">
+                  {calendarAllDays.map((day, index) => {
+                    if (!day) return <span className="w-8 h-8"></span>;
+                    return (
+                      <div
+                        key={index}
+                        className="text-sm content-center w-8 h-8 rounded-full cursor-pointer"
+                      >
+                        {day.getDate()}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+
+            {dateStep === "months" && (
+              <div className="grid grid-cols-3 gap-4">
+                {months.map((month, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="text-sm p-2 rounded-lg hover:bg-light-green hover:text-green cursor-pointer"
+                      onClick={() => {
+                        setMonthStore(index);
+                        setDateStep("years");
+                      }}
+                    >
+                      {month}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+
+            {dateStep === "years" && (
+              <div className="grid grid-cols-3 gap-4 text-center">
+                {yearOptions.map((year, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="text-sm p-2 rounded-lg hover:bg-light-green hover:text-green cursor-pointer"
+                      onClick={() => {
+                        setYearStore(year);
+                        setDateStep("days");
+                      }}
+                    >
+                      {year}
+                    </div>
+                  );
+                })}
+              </div>
+            )}
           </div>
         </div>
       )}
